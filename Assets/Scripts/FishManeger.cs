990fe54e77088123b8d936f6fc734eb6e2ddf7c4
teo.doutlikpuljic <teo.doutlikpuljic@elev.ga.lbs.se>
2025-10-16 15:49:36 +0200

    Fish0.1.2

--- Assets/Scripts/FishManeger.cs
+++ Assets/Scripts/FishManeger.cs
@@ -1,16 +1,37 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class FishManeger : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    
+    public GameObject[] fishTypes;
+    
+    public List<GameObject> fishOnScene;
+
+    public GameObject fishSpawn;
+    public GameObject leftFishSpawn;
+    public GameObject rightFishSpawn;
+
+    public void spawnFish()
     {
+        int FishToSpawn = Random.Range(0,fishTypes.Length);
+
+        int FishLeftOrRight = Random.Range(0, 1);
+
+        if(FishLeftOrRight == 0)
+        {
+            
+        }
+        else if (FishLeftOrRight == 1)
+        {
+
+        }
+
         
+
+        Instantiate(fishTypes[FishToSpawn], fishSpawn.transform.position, Quaternion.identity);
+
+        fishOnScene.Add(fishTypes[FishToSpawn]);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
