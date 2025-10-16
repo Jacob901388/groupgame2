e7ce0d539f89bdffab162453e6c9f089e8255d0d
BosnianFreak2K <kostic.bosko@outlook.com>
2025-10-16 15:50:41 +0200

    GameManager

---
+++ Assets/Bosko/Scripts/buttonScript.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.UI;
+public class buttonScript : MonoBehaviour
+{
+
+    [Header("Shop")]
+    public int shop1prize;
+    public Text shop1text;
+
+    public int shop2prize;
+    public Text shop2text;
+
+
+    [Header("Amount")]
+    public Text amount1Text;
+    public int amount1;
+    public float amount1Profit;
+
+    public Text amount2Text;
+    public int amount2;
+    public float amount2Profit;
+    
+    [Header("upgrades")]
+    public int upgradePrize;
+    public Text upgradeText;
+    
+  
+
+    void Update()
+    {
+        //Upgrade
+        upgradeText.text = "cost" + upgradePrize + " $";
+
+        //shop
+        shop1text.text = "Tier 1: " + shop1prize + " $: ";
+        shop2text.text = "Tier 2: " + shop1prize + " $: ";
+
+        //amounts
+        amount1Text = "Tier 1: "+ amount1+ " arts $: "+amount1Profit+"/s";
+
+    }
+
+    public void Upgrade()
+    {
+        if (currentScore >= upgradePrize)
+        {
+            currentScore -= upgradePrize;
+            hitPower *= 2;
+            upgradePrize *= 3;
+        }
+    }
+
+
+}
