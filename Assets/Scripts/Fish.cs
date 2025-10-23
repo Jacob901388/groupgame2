7b93b641fb8d04e4eada99f3e70c336c08b6fa28
teo.doutlikpuljic <teo.doutlikpuljic@elev.ga.lbs.se>
2025-10-23 16:35:47 +0200

    fish1.2

--- Assets/Scripts/Fish.cs
+++ Assets/Scripts/Fish.cs
@@ -1,19 +1,39 @@
 using UnityEngine;
+using System.Collections;
+using NUnit.Framework;
+using System.Collections.Generic;
 
 public class Fish : MonoBehaviour
 {
     [SerializeField] FishData fishData;
 
+    [SerializeField] float fishLifeDuration;
+
+    FishManeger fishManger;
     Rigidbody2D rb;
 
     void Start()
     {
+        fishManger = FindAnyObjectByType<FishManeger>();
         rb = GetComponent<Rigidbody2D>();
+
+        StartCoroutine(FishLifeDuration());
     }
 
     
     void Update()
     {
         rb.AddForce(transform.right * fishData.fishSpeed);
+    }
+
+    IEnumerator FishLifeDuration()
+    {
+
+        yield return new WaitForSeconds(fishLifeDuration);
+        
+        Destroy(gameObject);
+        GameObject currentFish = fishManger.fishOnScene[fishManger.FishToSpawn];
+        fishManger.fishOnScene.Remove(currentFish);
+
     }
 }
