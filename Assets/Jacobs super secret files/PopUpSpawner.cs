cb9ce235c1f0ee44f9003f64f7bd384c5c794c6b
Jacob901388 <jacobarmstrong2020@gmail.com>
2025-11-18 19:58:39 +0100

    nearly done with stuff

--- Assets/Jacobs super secret files/PopUpSpawner.cs
+++ Assets/Jacobs super secret files/PopUpSpawner.cs
@@ -1,16 +1,38 @@
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class PopUpSpawner : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] TextMeshProUGUI PopupText;
+    string Popup = "";
+    [SerializeField] string fishname;
+    [SerializeField] int amountoffish;
+    [SerializeField] Transform Startpos;
+    [SerializeField] Transform Endpos;
+    [SerializeField] Transform popuptarget;
+
+    bool canmove = false;
+    Vector3 velocity = Vector3.zero;
+    private void FixedUpdate()
     {
-        
+        Popup = $"you have cought {amountoffish} amount of {fishname}";
+
+        if(canmove == true)
+        {
+        popuptarget.position = Vector3.SmoothDamp(Startpos.position, Endpos.position, ref velocity, 0.1f);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdatePopup()
     {
-        
+        PopupText.text = Popup;
+        MovePopup();
+    }
+    public void MovePopup()
+    {
+        canmove = true;
     }
 }
+
